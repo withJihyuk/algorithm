377b9acfd8ca07528e648bbdb8121fef99752664
Jihyuk Lee <lead@sver.dev>
2024-04-23 05:17:24 -0700

    [Bronze V] Title: 크냐?, Time: 232 ms, Memory: 17972 KB -BaekjoonHub

---
+++ Java/백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,26 @@
+    import java.math.BigInteger;
+    import java.util.Scanner;
+
+    public class Main{
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int comment1 = 0;
+            int comment2 = 0;
+
+            while(sc.hasNextInt()) {
+                comment1 = sc.nextInt();
+                comment2 = sc.nextInt();
+
+                if(comment1 == 0 && comment2 == 0) {
+                    break;
+                }
+
+                if (comment1 > comment2) {
+                    System.out.println("Yes");
+                } else {
+                    System.out.println("No");
+                }
+            }
+
+        }
+    }
