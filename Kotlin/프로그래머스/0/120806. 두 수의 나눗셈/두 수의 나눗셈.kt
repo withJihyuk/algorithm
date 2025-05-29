0b856030ee45304a3a2b0aa7dd395c319ef18b8a
Jihyuk Lee <jihyuk1714@gmail.com>
2025-05-29 10:04:29 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 62 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+        answer = (num1.toDouble() / num2 * 1000).toInt()
+        return answer
+    }
+}
